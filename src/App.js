2f41974ffa8e777b0ee05f534adf286d3e73eedd
Simon Hoyos <shmesa23@gmail.com>
2018-02-04 17:35:58 -0500

    Create square using inline styling method

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const styles = {
+  box: {
+    width: 100,
+    height: 100,
+    border: '10px solid chocolate',
+    borderRadius: 10
+  }
+};
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <div
+        className="App"
+        style={styles.box}
+      />
     );
   }
 }
